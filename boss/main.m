d2be6d9343bc6730e8df21d67cf046d2f926c88a
sneixpy <davidasao@yahoo.com>
2014-09-05 21:29:49 -0700

    First Check in of all Xcode
    Adding all generated code

---
+++ boss/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  boss
+//
+//  Created by DavidAsao on 9/5/14.
+//  Copyright (c) 2014 Dr. Melody Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "app1AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([app1AppDelegate class]));
+    }
+}
---
+++ bossTests/bossTests.m
@@ -0,0 +1,34 @@
+//
+//  bossTests.m
+//  bossTests
+//
+//  Created by DavidAsao on 9/5/14.
+//  Copyright (c) 2014 Dr. Melody Yi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface bossTests : XCTestCase
+
+@end
+
+@implementation bossTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
